f6d7a40858e5870c446c81b840bd61e8c2b5f777
laolang2016 <xiaodaima2016@163.com>
2024-10-06 18:11:53 +0800

    cpp-base: msvc 特殊处理

--- cpp-base/include/janna/util/pch.h
+++ cpp-base/include/janna/util/pch.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#ifdef JANNA_LIB_UTIL_EXPORT
-    #define JANNA_LIB_UTIL __declspec(dllexport)
+#ifdef _MSC_VER
+    #ifdef JANNA_LIB_UTIL_EXPORT
+        #define JANNA_LIB_UTIL __declspec(dllexport)
+    #else
+        #define JANNA_LIB_UTIL __declspec(dllimport)
+    #endif
 #else
-    #define JANNA_LIB_UTIL __declspec(dllimport)
+    #define JANNA_LIB_UTIL
 #endif
